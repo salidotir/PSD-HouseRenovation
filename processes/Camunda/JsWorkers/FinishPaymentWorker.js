8bd7c338e1f590613d9f2d745bad98176530953f
behdadn77 <behdad.nemati1@gmail.com>
2023-06-24 22:47:21 +0200

    Fiexed IDs, added worker.js files

---
+++ processes/Camunda/JsWorkers/FinishPaymentWorker.js
@@ -0,0 +1,16 @@
+import { Client, logger } from "camunda-external-task-client-js";
+
+// configuration for the Client:
+//  - 'baseUrl': url to the Process Engine
+//  - 'logger': utility to automatically log important events
+const config = { baseUrl: "http://localhost:8080/engine-rest", use: logger };
+
+// create a Client instance with custom configuration
+const client = new Client(config);
+
+// susbscribe to the topic: 'creditScoreChecker'
+client.subscribe("creditScoreChecker", async function({ task, taskService }) {
+  // Put your business logic
+  // complete the task
+  await taskService.complete(task);
+});
