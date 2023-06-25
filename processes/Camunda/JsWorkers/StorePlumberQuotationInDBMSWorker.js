2af627cf43d7dcd40a11ce56906026e31c2d04b9
behdadn77 <behdad.nemati1@gmail.com>
2023-06-25 11:06:53 +0200

    Revert "restructured the JsWorkers dir"
    This reverts commit 83d7c5afa979135956ba1b49326cbc8b4209abb9.

--- processes/Camunda/JsWorkers/StorePlumberQuotationInDBMSWorker.js
+++ processes/Camunda/JsWorkers/StorePlumberQuotationInDBMSWorker.js
@@ -8,12 +8,9 @@ const config = { baseUrl: "http://localhost:8080/engine-rest", use: logger };
 // create a Client instance with custom configuration
 const client = new Client(config);
 
-// susbscribe to the topic: 'check-request-feasibility'
-client.subscribe("check-request-feasibility", async function({ task, taskService }) {
+// susbscribe to the topic: 'creditScoreChecker'
+client.subscribe("creditScoreChecker", async function({ task, taskService }) {
   // Put your business logic
-  var Is_feasible = math.random() >= 0.5;
-  const processVariables = new Variables();
-  processVariables.set("Is_feasible",Is_feasible);
   // complete the task
   await taskService.complete(task);
 });
