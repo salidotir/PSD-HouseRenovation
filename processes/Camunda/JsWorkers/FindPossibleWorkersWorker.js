c2a040aad62c8697aad2445d12a149931f9d40d0
Mohammad Hossein BehzadiFard <behzadi.hosein@yahoo.com>
2023-06-25 15:18:52 +0200

    Find possible workers debuged3

--- processes/Camunda/JsWorkers/FindPossibleWorkersWorker.js
+++ processes/Camunda/JsWorkers/FindPossibleWorkersWorker.js
@@ -39,8 +39,8 @@ client.subscribe('Find-possible-workers', async function ({ task, taskService })
 
     const plumber = ["P_GroupA", "P_GroupB", "P_GroupC", "P_GroupD", "P_GroupE", "P_GroupF"];
     //var Is_Plumber_Required = randomNumber1 > 3 ? true : false;
-    var Is_Plumber_Required = true;
-    console.log("Is_Plumber_Required: " + Is_Plumber_Required);
+    // var Is_Plumber_Required = true;
+    // console.log("Is_Plumber_Required: " + Is_Plumber_Required);
 
     const electrician = ["E_Group1", "E_Group2", "E_Group3", "E_Group4", "E_Group2", "E_Group3"]
     //var Is_Electrician_Required = randomNumber2 > 3 ? true : false;
@@ -62,7 +62,7 @@ client.subscribe('Find-possible-workers', async function ({ task, taskService })
 
     //Complete the task
 
-    processVariables.set('Is_Plumber_Required', Is_Plumber_Required);
+    //processVariables.set('Is_Plumber_Required', Is_Plumber_Required);
     processVariables.set('Is_Electrician_Required', Is_Electrician_Required);
     processVariables.set('Is_Constructor_Required', Is_Constructor_Required);
 
@@ -78,3 +78,4 @@ client.subscribe('Find-possible-workers', async function ({ task, taskService })
   })
 
 });
+//✖️ couldn't complete task efc8bf56-1356-11ee-b6e2-0242ac110002, EngineError: Response code 500 (); Error: Unknown property used in expression: ${Plumber}. Cause: Cannot resolve identifier 'Plumber'; Type: ProcessEngineException; Code: 0
